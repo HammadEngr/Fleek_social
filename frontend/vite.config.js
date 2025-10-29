937b88a2dc567fb2fe98f7858fa27d25649d94e3
HammadEngr <hammadahmed.engr@gmail.com>
2025-10-29 14:59:17 +0100

    vite config modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // equivalent to --host
+    watch: {
+      usePolling: true,
+      interval: 1000, // check for changes every second
+    },
+    port: 5173,
+  },
+});
