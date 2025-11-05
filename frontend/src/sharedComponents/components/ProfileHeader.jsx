c1686ea897315a72825d2ba9348725cc2658bd35
HammadEngr <hammadahmed.engr@gmail.com>
2025-11-05 14:38:31 +0100

    UPDATED_selfview header modified

--- frontend/src/sharedComponents/components/ProfileHeader.jsx
+++ frontend/src/sharedComponents/components/ProfileHeader.jsx
@@ -1,25 +1,40 @@
-import { Pencil, UserRoundPen } from "lucide-react";
-import { useRef } from "react";
-import Button from "../../ui/components/Button";
+import { Typography } from "antd";
+import { Camera, Pencil } from "lucide-react";
+import { useState } from "react";
 import styles from "../styles/ProfileHeader.module.css";
 
 function ProfileHeader({ selfView, editProfile }) {
+  const [expanded, setExpanded] = useState(false);
   return (
     <div className={styles.selfview_header}>
-      <div className={styles.p_pic}></div>
+      <div className={styles.p_pic}>
+        <div className={styles.p_cover}>
+          <img src="/cover.JPG" alt="" />
+          {selfView === true ? (
+            <span className={styles.edit_p_pic} onClick={editProfile}>
+              <Pencil strokeWidth={2} size={20} />
+            </span>
+          ) : null}
+          <div className={styles.p_dp}>
+            <img src="/hammad.png" alt="" />
+          </div>
+        </div>
+      </div>
       <div className={styles.p_details_wrapper}>
         <div className={styles.p_details}>
-          {selfView === true ? (
-            <Button
-              size="sm"
-              className={styles.edit_user}
-              onClick={editProfile}
-            >
-              <Pencil strokeWidth={1} />
-            </Button>
-          ) : null}
           <div className={styles.p_personal}>
-            <p className={styles.p_name}>Hammad Ahmed</p>
+            <p className={styles.p_name}>
+              Hammad Ahmed{" "}
+              {selfView === true ? (
+                <span
+                  size="sm"
+                  className={styles.edit_user}
+                  onClick={editProfile}
+                >
+                  <Pencil strokeWidth={1} size={16} />
+                </span>
+              ) : null}{" "}
+            </p>
             <p className={styles.p_profession}>Software Engineer</p>
           </div>
           <div className={styles.p_loc}>
@@ -31,7 +46,7 @@ function ProfileHeader({ selfView, editProfile }) {
           <div className={styles.p_network}>
             <p>
               {" "}
-              <span>1000</span> followers
+              <span>1000</span>followers
             </p>
             <p>
               {" "}
@@ -40,15 +55,30 @@ function ProfileHeader({ selfView, editProfile }) {
           </div>
         </div>
         <div className={styles.p_about}>
-          <p>
+          <Typography.Paragraph
+            ellipsis={{
+              rows: 4,
+              expandable: true,
+              symbol: "More",
+              expanded,
+              onExpand: (_, info) => setExpanded(info.expanded),
+            }}
+          >
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita
             alias velit blanditiis magni cupiditate impedit aut, fuga eligendi
             vero a quibusdam consequuntur laborum architecto rerum sunt amet,
             debitis dolorem. Facere. Quidem fugiat iste quos labore provident
             beatae ipsum, eveniet sequi, nihil, a ex praesentium magnam illo.
             Magnam minus ipsum animi placeat ut necessitatibus enim quibusdam
-            illum vitae modi? Iste, ex! Maxime reiciendis tempora amet.
-          </p>
+            illum vitae modi? Iste, ex! Maxime reiciendis tempora amet. Lorem
+            ipsum dolor sit, amet consectetur adipisicing elit. Expedita alias
+            velit blanditiis magni cupiditate impedit aut, fuga eligendi vero a
+            quibusdam consequuntur laborum architecto rerum sunt amet, debitis
+            dolorem. Facere. Quidem fugiat iste quos labore provident beatae
+            ipsum, eveniet sequi, nihil, a ex praesentium magnam illo. Magnam
+            minus ipsum animi placeat ut necessitatibus enim quibusdam illum
+            vitae modi? Iste, ex! Maxime reiciendis tempora amet.
+          </Typography.Paragraph>
         </div>
       </div>
     </div>
